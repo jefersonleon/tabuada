9568c33fc307f0df34ed89d1d1d65f3dd457cac4
Jeferson Leon <jefersonleonblue@gmail.com>
2020-05-19 21:10:20 -0300

    Aula LIVE dia 18/05 - Fazendo a tabuada utilizando o formulario

--- src/x/ExemploLoopDoWhile.java
+++ src/x/ExemploLoopDoWhile.java
@@ -1,4 +1,4 @@
-package exemploloopdowhile;
+package x;
 
 import javax.swing.JOptionPane;
 
@@ -11,13 +11,14 @@ public class ExemploLoopDoWhile {
     public static void main(String[] args) {
 
         int n, c;
-        c = 1;
+        c = 1; //inicializando a variavel que controla o fluxo de repetição
+        //fazendo a leitura do valor para a tabuada
         n = Integer.parseInt(JOptionPane.showInputDialog("Informe o número da tabuda:"));
         do {
             if (c == 1) {
                 System.out.println(" === TABUADA === ");
             }
-            System.out.println(c + " x " + n + " = " + (c * n));
+            System.out.println(c + " x " + n + " = " + (c * n));//gerando a tabuada
             c++;
         } while (c <= 10);
         System.out.println("=== FIM DA TABUADA === ");
